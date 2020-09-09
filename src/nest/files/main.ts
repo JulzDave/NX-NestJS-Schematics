c569c4e54c84d81c9ee3838814f0e6320ea48c29
Julian <juliandave1989@gmail.com>
2020-09-09 18:19:39 +0300

    enabled for loop dependency install. added stdout msg

--- src/nest/files/main.ts
+++ src/nest/files/main.ts
@@ -5,11 +5,41 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { start as startAPM } from 'elastic-apm-node';
+
+const PLUGIN_NAME = '<%= pluginName %>';
+const TLS_ACRONYM = 'tls';
+const TLS_VERSION = 'TLSv1';
+const NODE_TLS_REJECT_UNAUTHORIZED = 'NODE_TLS_REJECT_UNAUTHORIZED';
+// const APM_SERVER_URL = 'http://slpelastic-apm01:8200'; // TODO: Place inside the dedicated ENV files.
+const ZERO_STRING_LITERAL = '0';
+const API_STRING_LITERAL_UPPERCASED = 'API';
+const PLACEHOLDER_STRING_LITERAL = 'placeholder'; // TODO: For the Programmer: Address the placeholders and provide content.
+const SWAGGER_VERSION = '1.0.0'
+
 async function bootstrap() {
-    console.log('This is a custom made NestJS plugin!!!');
+    // startAPM({
+    //     serviceName: PLUGIN_NAME,
+    //     serverUrl: APM_SERVER_URL,
+    // });
+    require(TLS_ACRONYM).DEFAULT_MIN_VERSION = TLS_VERSION; // Allows work with WMB's SSL protocol.
+    process.env[NODE_TLS_REJECT_UNAUTHORIZED] = ZERO_STRING_LITERAL; // Allows work with WMB's SSL protocol.
     const app = await NestFactory.create(AppModule);
-    const globalPrefix = 'api';
+    const globalPrefix = PLUGIN_NAME;
     app.setGlobalPrefix(globalPrefix);
+    app.enableCors();
+    const swaggerOptions = new DocumentBuilder()
+        .setTitle(`${PLUGIN_NAME} ${API_STRING_LITERAL_UPPERCASED}`)
+        .setDescription(
+            `${PLUGIN_NAME} ${API_STRING_LITERAL_UPPERCASED} ${PLACEHOLDER_STRING_LITERAL}`,
+        )
+        .setVersion(SWAGGER_VERSION)
+        .build();
+    const document = SwaggerModule.createDocument(app, swaggerOptions, {
+        ignoreGlobalPrefix: true,
+    });
+    SwaggerModule.setup(API_STRING_LITERAL_UPPERCASED.toLowerCase(), app, document);
     const port = process.env.PORT || 3333;
     await app.listen(port, () => {
         Logger.log(
--- src/nest/index.ts
+++ src/nest/index.ts
@@ -16,6 +16,7 @@ import {
     // camelize,
     // underscore,
 } from '@angular-devkit/core/src/utils/strings';
+import { yellow, underline, bold } from 'chalk';
 import { strings } from '@angular-devkit/core';
 import { ISchema } from './schema';
 import { parseName } from '@schematics/angular/utility/parse-name';
@@ -28,6 +29,13 @@ import {
 console.log(parseName, buildDefaultPath);
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
+
+const DEPENDENCIES: [string, string][] = [
+    ['elastic-apm-node', '^3.6.1'],
+    ['@nestjs/swagger', '^4.5.7'],
+    ['swagger-ui-express', '^4.1.4'],
+    ['compression', '^1.7.4'],
+];
 
 export function nest(options: ISchema): Rule {
     return (tree: Tree, context: SchematicContext) => {
@@ -43,6 +51,7 @@ export function nest(options: ISchema): Rule {
         if (!foundPlugin) {
             throw new SchematicsException(`Plugin ${pluginName} not found`);
         }
+
         const defaultProjectPath = buildDefaultPath(foundPlugin);
         const parsedPath = parseName(defaultProjectPath, pluginName);
         const { name } = parsedPath;
@@ -57,13 +66,30 @@ export function nest(options: ISchema): Rule {
         ]);
         context.addTask(new NodePackageInstallTask());
         // context.addTask(new RunSchematicTask('add-dependencies', options), [installTaskId]);
-        const lodashDependency: NodeDependency = _nodeDependencyFactory(
-            'tiny',
-            '^0.0.10',
-        );
-        addPackageJsonDependency(tree, lodashDependency);
+        for (const dependency of DEPENDENCIES) {
+            const lodashDependency: NodeDependency = _nodeDependencyFactory(
+                dependency[0],
+                dependency[1],
+            );
+            addPackageJsonDependency(tree, lodashDependency);
+        }
+        console.log(yellow('                __          _____ '));
+        console.log(yellow('  _____ _____ _/  |______ _/ ____\\'));
+        console.log(yellow(' /     \\\\__  \\\\   __\\__  \\\\   __\\ '));
+        console.log(yellow('|  Y Y  \\/ __ \\|  |  / __ \\|  |   '));
+        console.log(yellow('|__|_|  (____  /__| (____  /__|   '));
+        console.log(yellow('      \\/     \\/          \\/       \n\n'));
+        console.log(yellow(bold(underline('This is an NX-NestJS dedicated plugin schematic\n'))))
+        console.log(yellow('* Address placeholders and provide useful content'))
+        console.log(yellow('* Confirm APM ports are open and ready.'))
+        console.log(yellow('* Subsequently, uncomment the APM configurations in the main.ts file.\n\n'))
+
+
+
+
         return mergeWith(sourceParameterizedTemplates)(tree, context);
     };
+
     function _nodeDependencyFactory(
         packageName: string,
         version: string,
